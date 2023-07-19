f7c80eeecde030ab353fb0cd571568b523ae606c
Raj <rajesh.mattaparthi824@gmail.com>
2023-07-19 16:49:58 +0530

    variables

---
+++ Practice2/src/Variables.java
@@ -0,0 +1,22 @@
+
+public class Variables {
+    //instance variables
+	int c=35;
+	int d=20;
+	//static variables
+	static int e=60;
+	static int g=50;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//local variables
+		Variables f=new Variables();
+		int a=95;
+		int b=5;
+		System.out.println(a+b);
+		System.out.println(f.c+f.d);
+		System.out.println(e+g);
+		
+
+	}
+
+}
